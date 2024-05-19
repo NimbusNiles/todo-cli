ce73ca1ba155f02095e041d190224dd3d864e333
NimbusNiles <29178202+NimbusNiles@users.noreply.github.com>
2024-05-19 17:34:49 +0200

    Move todo cli to separate folder

--- todo/todo_cli.py
+++ todo/todo_cli.py
@@ -8,14 +8,14 @@ def print_hello() -> None:
     print("Hello world!")
 
 
-def main() -> None:
+def main(argv: list[str] | None = None) -> None:
     """Main method parsing the arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-hi", "--hello", help="Print hello world.", action="store_true"
     )
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     if args.hello:
         print_hello()
