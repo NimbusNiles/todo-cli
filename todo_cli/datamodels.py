8dd8c2a8910a6a571c219e1a8b83fa9016cb3742
NimbusNiles <29178202+NimbusNiles@users.noreply.github.com>
2024-05-20 19:07:13 +0200

    Create subtask datamodel

--- todo_cli/datamodels.py
+++ todo_cli/datamodels.py
@@ -1,8 +1,9 @@
 """Class to represent ToDo items."""
 
 from dataclasses import dataclass
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
@@ -19,7 +20,23 @@ class Task(Base):
     id: Mapped[int] = mapped_column(primary_key=True, init=False)
     position: Mapped[int]
     text: Mapped[str]
+    subtasks: Mapped[list["Subtask"]] = relationship(
+        back_populates="task", cascade="all, delete-orphan", init=False
+    )
     status: Mapped[str] = mapped_column(default="To Do")
 
     def pretty_string(self) -> str:
         return f"│ {self.position}  {self.text:50} {self.status:^11} │"
+
+
+class Subtask(Base):
+    """Class representing a subtask of the main task of the todo list."""
+
+    __tablename__ = "subtask"
+
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    position: Mapped[int]
+    text: Mapped[str]
+    task: Mapped[Task] = relationship(back_populates="subtasks")
+    task_id: Mapped[int] = mapped_column(ForeignKey(Task.id))
+    status: Mapped[str] = mapped_column(default="To Do")
